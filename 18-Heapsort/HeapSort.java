092df84b67b3dd1969f44c8b2000cc1097d7fa2a
Max Zhou <ball908765@gmail.com>
2014-05-10 18:54:23 -0400

    works!

--- 18-Heapsort/HeapSort.java
+++ 18-Heapsort/HeapSort.java
@@ -1,3 +1,19 @@
-public class HeapSort(){
-    
+import java.util.*;
+
+public class HeapSort{
+    public static void sort(int[] nums){
+	MyHeap heap = new MyHeap(nums);
+	int[] temp = new int[heap.size()];
+	for (int i = 0; i < temp.length; i++){
+	    nums[i] = heap.removeMax();
+	}
+    }
+
+    public static void main (String[] args){
+	int[] blargh = {1,5,3,7,45,2,-6,-6,0};
+	System.out.println("unsorted: " + Arrays.toString(blargh)); 
+	sort(blargh);
+	System.out.println("sorted: " + Arrays.toString(blargh));
+	     
+    }
 }
--- 18-Heapsort/MyHeap.java
+++ 18-Heapsort/MyHeap.java
@@ -1,4 +1,5 @@
 import java.lang.*;
+import java.util.*;
 
 public class MyHeap{
     private int[] values;
@@ -9,12 +10,15 @@ public class MyHeap{
     }
     
     public MyHeap(int[] nums){
+	size = 0;
 	values = new int[nums.length+1];
-	values[0] = Integer.MAXVALUE;
-	for (int i = 0; i < nums.size; i++){
-	    values[i+1] = nums[i];
+	values[0] = Integer.MAX_VALUE;
+	for (int i = 0; i < nums.length; i++){
+	    insert(nums[i]);
+	    size++;
 	}
-	size = nums.length;
+	//size = nums.length;
+	System.out.println(Arrays.toString(values));
     }
 
     private void swap(int first, int second){ //takes indicies
@@ -24,30 +28,46 @@ public class MyHeap{
 	
     }
 
+    public int size(){
+	return size;
+    }
+
     public void insert(int num){
 	int loc = size+1;
 	values[loc] = num;
-	while (values[loc/2] < num){
-	    swap(loc/2, loc);
-	    loc = loc/2;
+	while (values[loc/2] < values[loc]){ //can swap up
+	    swap(loc/2, loc); //swap!
+	    loc = loc/2; //move loc to new location
 	}	
     }
 
-    public int removeMax(){
+    public int removeMax(){ 
 	int loc = size;
-	int removed = values[1];
+	int removed = values[1]; //the left/right side swapping might miss the actual maximum value
 	swap(loc, 1); //root is at 1!!
-	loc = 1; //where node wnet
-	while ((loc <= size * 2) && (values[loc] > values[loc*2] || values[loc] > values[loc*2+1])){
-	    if (values[loc*2] > values[loc*2+1]){
-		swap(loc, loc*2);
-		loc = loc*2;
+	values[loc] = Integer.MIN_VALUE;
+	size--; //this pretty much deletes the node
+	loc = 1; //where the swapped node went
+	
+	//	System.out.println(removed);
+
+	while (loc <= size / 2) //time to swap it down
+	    if (values[loc] < values[loc*2] || values[loc] < values[loc*2+1]){ //check if it can still be swapped
+		if (values[loc*2] > values[loc*2+1]){ //left side is bigger
+		    swap(loc, loc*2); //swap left
+		    loc = loc*2; //move loc to new position
+		}
+		else{
+		    swap(loc, loc*2+1); //swap right
+		    loc = loc*2+1;
+		}
+		//		System.out.println(Arrays.toString(values));
 	    }
+
 	    else{
-		swap(loc, loc*2+1);
-		loc = loc*2+1;
+		break; //quit if it's in its normal position
 	    }
-	}
+	//	System.out.println(Arrays.toString(values));
 	return removed;
     }
 }
